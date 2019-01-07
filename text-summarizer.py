ad9abbe1518eb5093c19b9e7a9dcc8ad72c0ddfd
Steven Beales <stevenbeales@gnmail.com>
2019-01-07 13:01:59 -0500

    Adds support for parsing HTML files

--- text-summarizer.py
+++ text-summarizer.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+"""Summarizes text retrieved from a text file or HTML page."""
+
 import argparse
 
 from collections import defaultdict
@@ -12,29 +16,85 @@ from string import punctuation
 
 import textwrap
 
+import bs4 as bs
+import urllib.request
+import re
+
 
 def main():
+
     args = parse_arguments()
-    data = ""
 
-    try:
-        with open(args.filepath, "r") as file:
-            data = file.read()
+    raw_data = parse_data_from_input(args.filepath)
 
-    except IOError:
-        print(
-            f"Fatal Error: File ({args.filepath}) could not be located or is not readable."
-        )
-        exit()
+    content = sanitize_input(raw_data)
 
-    content = sanitize_input(data)
     sentence_tokens, word_tokens = tokenize_content(content)
     sentence_ranks = score_tokens(word_tokens, sentence_tokens)
 
     return summarize(sentence_ranks, sentence_tokens, args.length)
 
 
+def parse_data_from_input(source):
+    try:
+        raw_data = parse_data_from_file(source)
+    except IOError:
+        try:
+            raw_data = parse_data_from_url(source)
+        except Exception:
+            print(
+                f"Fatal Error: File ({source}) could not be located or is not readable."
+            )
+            exit()
+
+    return raw_data
+
+
+def parse_data_from_file(file_name):
+    """Gets raw data from local file
+
+    Keyword arguments:
+
+    file_name -- local file name containing text e.g. ./input.txt
+    """
+
+    raw_data = ""
+
+    with open(file_name, "r") as file:
+        raw_data = file.read()
+
+    return raw_data
+
+
+def parse_data_from_url(url):
+    """Gets raw HTML data from URL
+
+    Keyword arguments:
+
+    url -- HTML page e.g. http://www.google.com
+    """
+
+    scraped_data = urllib.request.urlopen(url)
+    article = scraped_data.read()
+
+    parsed_article = bs.BeautifulSoup(article, 'lxml')
+
+    paragraphs = parsed_article.find_all('p')
+    raw_data = ""
+    for para in paragraphs:
+        raw_data += para.text
+
+    return raw_data
+
+
 def parse_arguments():
+    """Accepts filepath and length as command line arguments.
+
+    Keyword arguments:
+    filepath -- fully qualified local file name or fully qualified URL
+    length -- umber of sentences to return in summarization (default 4)
+    """
+
     parser = argparse.ArgumentParser()
     parser.add_argument("filepath", help="File name of text to summarize")
     parser.add_argument(
@@ -45,21 +105,46 @@ def parse_arguments():
     return args
 
 
-def sanitize_input(data):
+def sanitize_input(raw_data):
+    """Removes special characters from raw data.
+
+    Keyword arguments:   
+        raw_data -- raw file or URL text
+    """
+
     replace = {ord("\f"): " ", ord("\t"): " ", ord("\n"): " ", ord("\r"): None}
 
-    return data.translate(replace)
+    # remove formfeeds, tabs and carriage returns
+    content = raw_data.translate(replace)
+
+    content = re.sub(r'\[[0-9]*\]', ' ', content)  # remove [n] style footnotes
+    content = re.sub(r'\s+', ' ', content)  # remove multiple spaces
+
+    return content
 
 
-def tokenize_content(content):
+def tokenize_content(clean_content):
+    """Removes English stop words and tokenizes content.
+
+    Keyword arguments:   
+        clean_content -- sanitized input data 
+    """
+
     stop_words = set(stopwords.words("english") + list(punctuation))
-    words = word_tokenize(content.lower())
+    words = word_tokenize(clean_content.lower())
 
-    return [sent_tokenize(content), [word for word in words if word not in stop_words]]
+    return [sent_tokenize(clean_content), [word for word in words if word not in stop_words]]
 
 
-def score_tokens(filterd_words, sentence_tokens):
-    word_freq = FreqDist(filterd_words)
+def score_tokens(filtered_words, sentence_tokens):
+    """Scores tokens by word frequency.
+
+    Keyword arguments:
+        filtered_words -- tokenized words
+        sentence_tokens -- tokenized sentences
+    """
+
+    word_freq = FreqDist(filtered_words)
 
     ranking = defaultdict(int)
 
@@ -72,6 +157,14 @@ def score_tokens(filterd_words, sentence_tokens):
 
 
 def summarize(ranks, sentences, length):
+    """Puts highest ranked sentences into summary.
+
+    Keyword arguments:
+        ranks -- ranked list
+        sentences -- tokenized sentences
+        length -- number of sentences in summary
+    """
+
     if int(length) > len(sentences):
         print(
             "Error, more sentences requested than available. Use --l (--length) flag to adjust."
@@ -84,14 +177,25 @@ def summarize(ranks, sentences, length):
     return " ".join(final_sentences)
 
 
-if __name__ == "__main__":
-    summary = main()
+def word_wrap_summary(summary):
+    """Word wraps raw text at column 80
+
+    Keyword arguments:
+
+    summary - - text to word wrap
+    """
+
     wrapper = textwrap.TextWrapper(width=80,
                                    initial_indent=" " * 4,
                                    subsequent_indent=" " * 4,
                                    break_long_words=False,
                                    break_on_hyphens=False)
-    word_wrapped_summary = wrapper.fill(summary)
+    return wrapper.fill(summary)
+
+
+if __name__ == "__main__":
+    summary = main()
+    formatted_summary = word_wrap_summary(summary)
     with open("summary.txt", "w") as text_file:
-        text_file.write(word_wrapped_summary)
-    print(word_wrapped_summary)
+        text_file.write(formatted_summary)
+    print(formatted_summary)
